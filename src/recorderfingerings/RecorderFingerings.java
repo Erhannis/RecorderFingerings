9b2562e4b42c71a14462dda936bb05ebd4453cf5
Matthew Ewer <Ewer.Matthew@gmail.com>
2013-12-11 20:43:59 -0800

    Oh, I should probably version this.  This is a program to turn MIDI files into video of recorder fingerings.

---
+++ src/recorderfingerings/RecorderFingerings.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package recorderfingerings;
+
+/**
+ *
+ * @author mewer
+ */
+public class RecorderFingerings {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        RFMainWindow main = new RFMainWindow();
+        main.setVisible(true);
+    }
+}
